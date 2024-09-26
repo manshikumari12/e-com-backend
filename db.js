22332efe01cf94991e0207ad29f6358ac72c3f5f
manshikumari <manshisbp@gmail.com>
2024-09-25 22:08:23 -0700

    update

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
 
-const connection=mongoose.connect("mongodb+srv://manshisbp:manshi@cluster0.lyyjl0e.mongodb.net/ecom?retryWrites=true&w=majority&appName=Cluster0")
+const connection=mongoose.connect("mongodb+srv://manshisbp:manshi@cluster0.lyyjl0e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 module.exports={ connection}
